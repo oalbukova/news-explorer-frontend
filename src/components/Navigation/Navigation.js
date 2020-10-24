008d0a37cf99273857a77d6650e2d839ee90acdc
Olga Albukova <o.albukova@gmail.com>
2020-10-24 05:20:43 +0300

    fix nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,7 +8,7 @@ function Navigation() {
 
   return (
     <nav className="navigation">
-      <NavLink exact to="/" activeClassName="navigation__link_active" className='navigation__link' style={isBackgroundWhite ? blackText : whiteText}>Главная</NavLink>
+      <NavLink exact to="/" activeClassName="navigation__link_active" className='navigation__link'>Главная</NavLink>
       <NavLink exact to="/saved-news" activeClassName="navigation__link_active" className="navigation__link">Сохранённые статьи</NavLink>
       <button className="navigation__button">Грета
         <img className='navigation__signout' src={signOut} alt='Выход'/></button>
